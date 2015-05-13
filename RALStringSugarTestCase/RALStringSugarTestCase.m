01e3e08c54eca65df1dac4efed9922d79f4b46f7
Wei-Chen Ling <riddle.apple@gmail.com>
2015-05-13 21:53:30 +0800

    add TestCase files

---
+++ RALStringSugarTestCase/RALStringSugarTestCase.m
@@ -0,0 +1,30 @@
+//
+//  RALStringSugarTestCase.m
+//  RALStringSugarTestCase
+//
+//  Created by Riddle Ling on 2015/5/13.
+//  Copyright (c) 2015年 Riddle Ling. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+@interface RALStringSugarTestCase : XCTestCase
+
+@end
+
+@implementation RALStringSugarTestCase
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+
+
+@end
